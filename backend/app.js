c99f56fecedd410cfb12902cdfb6ea82eff3dc28
Lucas Balland <lucasballand101@gmail.com>
2022-12-08 14:50:31 +0100

    Nouveau repo
    Nouveau repo

---
+++ backend/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
+
+// Ne pas oublier d'enlever les guillemets autour de la chaîne de connexion et de remplacer les valeurs par les bonnes valeurs pour votre base de données MongoDB Atlas
+
+mongoose.connect('mongodb+srv://LucasP6:LucasBA@cluster0.xiqi6ga.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app;
